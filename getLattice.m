6ad243020bba4990f9fdf51e35b4b060eb6198ba
firdausazizi <17b2196@ubd.edu.bn>
2022-09-30 15:27:31 +0800

    Proper comments in getLattice added

--- getLattice.m
+++ getLattice.m
@@ -1,4 +1,15 @@
 function L = getLattice(type, h, w)
+  % This function file generates a checkerboard pattern of dimensions h*w
+  %   in the form of a sparse matrix
+  %
+  % Input:
+  %   - type -- Single character from {'A','B'}
+  %             This will decide whether the lattice starts from 0 or 1
+  %   - h    -- Integer - Height of the output lattice.
+  %   - w    -- Integer - Width of the output lattice.
+  %
+  % Output:
+  %   - L    -- Sparse matrix of size h by w with the pattern show below
   switch type
     case 'A'
       L = repmat(sparse([1 0; 0 1]), ceil(h/2), ceil(w/2))(1:h, 1:w);
